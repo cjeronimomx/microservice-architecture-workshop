8833a8e8512b324c41f657daf32215aa25e109a9
crisanto.jeronimo.next <crisanto.jeronimo.next@bbva.com>
2021-03-11 08:37:32 -0600

    Modify properties file structure

--- demo-config-server/src/main/java/com/demo/demo/config/DemoConfigServer.java
+++ demo-config-server/src/main/java/com/demo/demo/config/DemoConfigServer.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-
+//TODO: 0. Add banner.txt file
 //TODO: 1. Add @EnableConfigServer annotation
 @SpringBootApplication
 @EnableConfigServer
